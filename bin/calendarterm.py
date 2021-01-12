dbca80d704d3c6dcc8dbac60e1a59861a449d9ce
Antonio Cuni <anto.cuni@gmail.com>
2021-01-12 10:53:01 +0100

    calendarterm tweaks

--- bin/calendarterm.py
+++ bin/calendarterm.py
@@ -14,11 +14,43 @@ def conky_color(col, line):
 def dummy_color(col, line):
     return line
 
+def ansi_color(col, line):
+    return Color.set(col, line)
+
+class Color:
+    black = '30'
+    darkred = '31'
+    darkgreen = '32'
+    brown = '33'
+    darkblue = '34'
+    purple = '35'
+    teal = '36'
+    lightgray = '37'
+    darkgray = '30;01'
+    red = '31;01'
+    green = '32;01'
+    yellow = '33;01'
+    blue = '34;01'
+    fuchsia = '35;01'
+    turquoise = '36;01'
+    white = '37;01'
+
+    @classmethod
+    def set(cls, color, string):
+        try:
+            color = getattr(cls, color)
+        except AttributeError:
+            pass
+        return '\x1b[%sm%s\x1b[00m' % (color, string)
+
+
+
 def main(conky):
     if conky:
         color = conky_color
     else:
-        color = dummy_color
+        #color = dummy_color
+        color = ansi_color
 
     start = datetime.date.today()
     events = icalevents.events(URL, start=start)
