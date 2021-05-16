5f98b2c41ead34903eeeb141f19a8c25242211c8
Antonio Cuni <anto.cuni@gmail.com>
2021-05-16 11:26:08 +0200

    some notes about xinput

--- bin/mouse-reset.sh
+++ bin/mouse-reset.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo modprobe -r psmouse && sleep 2 && sudo modprobe psmouse
+
+# NOTE: you can also pass numerical ids instead of the full name
+# xinput test "Logitech USB Optical Mouse"
+# xinput disable 'Logitech USB Optical Mouse'
+# xinput enable 'Logitech USB Optical Mouse'
+
+
