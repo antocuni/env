49a96c3f8e8527493dd4fb9e359e1be1b44b428a
Antonio Cuni <anto.cuni@gmail.com>
2021-05-16 11:27:00 +0200

    make it possible to paste an existing image

--- bin/pastescreen.py
+++ bin/pastescreen.py
@@ -1,17 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 
 import os
 import random
 import string
+import sys
 
-letters = string.lowercase + string.uppercase + string.digits
-ID = [random.choice(letters) for i in range(8)]
-fname = '%s.png' % ''.join(ID)
+if len(sys.argv) == 2:
+    # existing file
+    fpath = sys.argv[1]
+    _, fname = os.path.split(fpath)
+else:
+    letters = string.lowercase + string.uppercase + string.digits
+    ID = [random.choice(letters) for i in range(8)]
+    fname = '%s.png' % ''.join(ID)
 
-fpath = '/tmp/pastescreen/%s' % fname
-os.system('mkdir -p /tmp/pastescreen')
-os.system('import %s' % fpath)
-os.system('echo -n wait | xsel -i -b')
+    fpath = '/tmp/pastescreen/%s' % fname
+    os.system('mkdir -p /tmp/pastescreen')
+    os.system('import %s' % fpath)
+    os.system('echo -n wait | xsel -i -b')
+
 os.system('scp %s antocuni.eu:www/antocuni.eu/misc/img' % fpath)
 
 URL = 'http://antocuni.eu/misc/img/%s' % fname
