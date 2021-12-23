c47a48b10e9ba7d3c8ff02150cacba5ce3cec5f2
Antonio Cuni <anto.cuni@gmail.com>
2021-12-23 11:02:23 +0100

    kill the useless no_switch logic

--- bin/super.py
+++ bin/super.py
@@ -12,29 +12,24 @@ TELEGRAM = 'Telegram.TelegramDesktop'
 
 WLIST = wmctrl.Window.list()
 
-def show(cls, i, no_switch, spawn=None):
+def show(cls, i=0, spawn=None):
     """
     Activate the i-th window of the given class.
-
     If there are less windows than i, activate the first.
 
     The logic is to prefer:
       1. sticky windows first
       2. windows which are on the active desktop
-      3. if there is NO window of that class on the active desktop, then consider also
-         the windows on other desktops
+
     """
     desktop = wmctrl.Desktop.get_active()
     # try to find the windows on the current desktop
     wlist = ([w for w in WLIST if w.wm_class == cls and w.desktop == -1] +
              [w for w in WLIST if w.wm_class == cls and w.desktop == desktop.num])
-    if not wlist and not no_switch:
-        # try to find the windows on all desktops
-        wlist = [w for w in WLIST if w.wm_class == cls]
     if not wlist:
+        # no windows found
         if spawn:
             return os.system(spawn)
-        # no windows found, give up
         print 'No windows found: %s' % cls
         return 1
     #
@@ -94,27 +89,26 @@ def main():
     arg = sys.argv[1]
     no_switch = '--no-switch' in sys.argv
 
-    if   arg == 'emacs': return show('emacs.Emacs', 0, no_switch)
-    elif arg == 'term':  return show('gnome-terminal-server.Gnome-terminal', 0, no_switch=True, spawn='autoterm')
-    elif arg == '1':     return show(CHROME, 0, no_switch=True, spawn='/home/antocuni/env/apps/my-google-chrome.desktop')
-    elif arg == '2':     return show(CHROME, 1, no_switch)
-    elif arg == '3':     return show(CHROME, 'cycle', no_switch)
-    elif arg == 'q':     return show('web.whatsapp.com.Google-chrome', 0, no_switch)
-    elif arg == 'w':     return show(TELEGRAM, 0, no_switch)
-    #elif arg == 'e':     return show('mail.google.com.Google-chrome', 0, no_switch)
-    elif arg == 'a':     return show(MATTERMOST, 0, no_switch)
-    elif arg == 's':     return show('hexchat.Hexchat', 0, no_switch)
+    if   arg == 'emacs':   return show('emacs.Emacs')
+    elif arg == 'term':    return show('gnome-terminal-server.Gnome-terminal', spawn='autoterm')
+    elif arg == '1':       return show(CHROME, 0)
+    elif arg == '2':       return show(CHROME, 1)
+    elif arg == '3':       return show(CHROME, 'cycle')
+    elif arg == 'q':       return show('web.whatsapp.com.Google-chrome')
+    elif arg == 'w':       return show(TELEGRAM)
+    #elif arg == 'e':      return show('mail.google.com.Google-chrome', 0, no_switch)
+    elif arg == 'a':       return show(MATTERMOST)
+    elif arg == 's':       return show('hexchat.Hexchat')
     elif arg == 'prtscrn': return take_screenshot()
-    elif arg == 'esc':   return show('goldendict.GoldenDict', 0, no_switch, spawn='goldendict')
-    elif arg == 'F1':
-        show_zeal()
+    elif arg == 'esc':     return show('goldendict.GoldenDict', spawn='goldendict')
+    elif arg == 'F1':      return show_zeal()
     elif arg == 'F2':
         os.system('/home/antocuni/env/conky/myconky.py')
         os.system('reposition-windows.py')
         return os.system('kbd')
-    elif arg == 'F3':    return os.system('kbd')
-    elif arg == 'F11':   return os.system('reposition-windows.py emergency')
-    elif arg == 'F12':   return os.system('auto-xrandr.sh')
+    elif arg == 'F3':      return os.system('kbd')
+    elif arg == 'F11':     return os.system('reposition-windows.py emergency')
+    elif arg == 'F12':     return os.system('auto-xrandr.sh')
     else:
         print 'Unknown arg:', arg
 
